d81e5c57583c9f0dfb90d0f45a9ba935357feb70
guillaume-gomez <guillaume.gomez846@gmail.com>
2025-02-05 12:42:47 +0100

    test my algorithm about cutting the walls

--- src/twentieth/Room.tsx
+++ src/twentieth/Room.tsx
@@ -52,10 +52,14 @@ function Room ({
             return coords;
         });
 
+        console.log(geometriesData);
+
 
         if(holes[holes.length -1].end != width) {
-           geometriesData = [...geometriesData, [hole.end, width]]
+           geometriesData = [...geometriesData, [holes[holes.length -1].end, width]]
         }
+
+        console.log(geometriesData)
 
         return geometriesData.map(([start, end]) => {
             return <mesh
@@ -68,49 +72,7 @@ function Room ({
 
     }
 
-
-    if(holes.length !== 0) {
-        return (
-        <group position={position}>
-            {/*floor*/}
-            <mesh
-                position={[0,0,0]} rotation={[-Math.PI/2, 0, 0]}
-            >
-              <planeGeometry args={[width, height]} />
-              <meshStandardMaterial color={0x400000} />
-            </mesh>
-
-            <mesh
-                position={[0, wallHeight/2, height/2 - depth/2]}
-                material={material}
-                boxGeometry
-            >
-              <boxGeometry args={[width, wallHeight, depth]}/>
-            </mesh>
-
-            <mesh
-                position={[0, wallHeight/2, -(height/2 - depth/2)]}
-                material={material}
-            >
-              <boxGeometry args={[width, wallHeight, depth]}/>
-            </mesh>
-
-            <mesh
-                position={[width/2 - depth/2, wallHeight/2,0]}
-                material={material}
-            >
-              <boxGeometry args={[depth, wallHeight, height - 2]}/>
-            </mesh>
-
-            <mesh
-                position={[-(width/2 - depth/2), wallHeight/2,0]}
-                material={material}
-            >
-              <boxGeometry args={[depth, wallHeight, height - 2]}/>
-            </mesh>
-        </group>
-        )
-    }
+    console.log(buildNorthAndSouth([{begin:7, end: 8}, {begin:3, end: 5}]));
 
     return (
         <group position={position}>
@@ -155,3 +117,37 @@ function Room ({
 }
 
 export default Room;
+/*
+
+
+0 ----- -----           10
+0 --- -- -----          10
+0 --- --------          10
+
+
+0-3 3-5 5-10
+
+0 - width   [0 - width]
+start - end [3-5]
+end - width [5-width]
+
+[0,3,5,10]
+
+[3,5]
+
+begin 0,
+end = null
+
+begin 0
+end 3->it.begin
+begin =  it.end
+
+
+if(begin != width) {
+    it.end
+    width
+}
+
+
+*/
+
