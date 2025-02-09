fe7e08b1a41fb77e6e0ea81bfeb0909b37df3cdc
guillaume-gomez <guillaume.gomez846@gmail.com>
2025-02-09 22:38:27 +0100

    linter

--- src/third/App.tsx
+++ src/third/App.tsx
@@ -3,6 +3,7 @@ const colours = ["#3D348B", "#7678ED", "#F7B801", "#F18701", "#F35B04", "#FFFFFF
 const shadowColors = ["#0C0F0A", "#544B3D"]
 const FORTY_TWO = 42;
 const ANIMATION_DURATION = 15000;
+
 export default function App() {
   const canvasRef = useRef<HTMLCanvasElement>(null);
   const launch = useRef<boolean>(false);
@@ -15,6 +16,12 @@ export default function App() {
     canvasRef.current.width = document.body.offsetWidth - 15;
     canvasRef.current.height = document.body.offsetHeight - 15;
     const context = canvasRef.current.getContext("2d");
+
+    if(!context) {
+      console.log("something went wrong");
+      return;
+    }
+
     context.clearRect(0,0, canvasRef.current.width, canvasRef.current.height);
     context.shadowColor = shadowColors[ Math.floor(Math.random() * shadowColors.length )];
     const shadow = 8;
@@ -25,17 +32,17 @@ export default function App() {
 
     for(let it = 0; it < numberOfCircles; it++) {
       setTimeout(() => {
-        const x = Math.random() * canvasRef.current.width;
-        const y = Math.random() * canvasRef.current.height;
+        const x = Math.random() * canvasRef!.current!.width;
+        const y = Math.random() * canvasRef!.current!.height;
         const radius = ((Math.random() * FORTY_TWO * 1.618033989) + 10);
-        context.beginPath();
-        context.arc(x, y, radius, 0, 2 * Math.PI, false);
-        context.fillStyle = colours[Math.floor(colours.length * Math.random())];
-        context.fill();
-        context.lineWidth = 5;
-        context.strokeStyle = '#333333';
-        context.stroke();
-      }, [ ANIMATION_DURATION * (it / (numberOfCircles)) ]);
+        context!.beginPath();
+        context!.arc(x, y, radius, 0, 2 * Math.PI, false);
+        context!.fillStyle = colours[Math.floor(colours.length * Math.random())];
+        context!.fill();
+        context!.lineWidth = 5;
+        context!.strokeStyle = '#333333';
+        context!.stroke();
+      }, ANIMATION_DURATION * (it / (numberOfCircles)) );
     }
   }, [canvasRef.current]);
   return (<canvas ref={canvasRef} />)
