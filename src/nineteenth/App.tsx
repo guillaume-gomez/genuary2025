2f00576b5c48ce93ff4e0648c06cca23b26250b0
guillaume-gomez <guillaume.gomez846@gmail.com>
2025-05-21 13:56:31 +0200

    bump tailwind and install daisy ui

--- src/nineteenth/App.tsx
+++ src/nineteenth/App.tsx
@@ -1,9 +1,11 @@
 import Sketch from "./Sketch";
+import Sketch2 from "./Sketch2";
 
 function App() {
   return (
-    <div className="w-full h-screen">
+    <div className="w-full h-full flex flex-col gap-5">
       <Sketch />
+      <Sketch2 />
     </div>
   )
 }
