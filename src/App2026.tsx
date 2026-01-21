b54585425879bbc2b56ee72c1f043b33e85ea55c
Guillaume Gomez <guillaume.gomez846@gmail.com>
2026-01-21 16:47:39 +0100

    update text to prepare new things

--- src/App2026.tsx
+++ src/App2026.tsx
@@ -2,7 +2,8 @@ function App2026() {
   
   return (
     <div className="flex flex-col h-full gap-2 bg-neutral-900 text-white p-4">
-      project for the 2026 version  
+      <p>project for the 2026 version<p>
+        For now nothing :)
     </div>
   )
 }
