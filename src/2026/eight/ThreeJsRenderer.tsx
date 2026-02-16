4fc24c51d926dd7422096c893361caaee996b101
guillaume-gomez <guillaume.gomez846@gmail.com>
2026-02-16 22:32:21 +0100

    start

--- src/2026/eight/ThreeJsRenderer.tsx
+++ src/2026/eight/ThreeJsRenderer.tsx
@@ -1,15 +1,18 @@
 import { useRef, Suspense } from 'react';
 import { useFullscreen } from "rooks";
 import { Canvas } from '@react-three/fiber';
-import { 
+import {
   Float,
   ContactShadows,
   OrbitControls,
   GizmoHelper,
-  GizmoViewport, 
+  GizmoViewport,
   Stats,
-  Environment
+  Environment,
+  Outlines,
+  Fog,
 } from '@react-three/drei';
+import { EffectComposer, Bloom, ToneMapping } from '@react-three/postprocessing';
 import FallBackLoader from "../../2025/first/FallBackLoader";
 
 
@@ -36,18 +39,40 @@ function ThreejsRenderer({
         <Suspense fallback={<FallBackLoader/>}>
           <ambientLight intensity={0.5 * Math.PI} />
           <Environment preset="night"/>
-          <Striplight position={[10, 2, 0]} scale={[1, 3, 10]} />
-          <Striplight position={[-10, 2, 0]} scale={[1, 3, 10]} />
+          <fog attach="fog" color="violet" near={0} far={20 * 2.25} />
+          {/*<Striplight position={[10, 2, 0]} scale={[1, 3, 10]} />
+          <Striplight position={[-10, 2, 0]} scale={[1, 3, 10]} />*/}
 
           <group position={[0, -1.5, 0]}>
-          <Float position={[0, 2.15, 0]} speed={2} rotationIntensity={2} floatIntensity={2}>
-            <mesh castShadow receiveShadow>
-              <torusKnotGeometry args={[1, 0.25, 256, 24, 1, 3]} />
-              <meshStandardMaterial color="white" roughness={0.1} metalness={0.925} />
-            </mesh>
-          </Float>
-          <ContactShadows scale={10} blur={3} opacity={0.25} far={10} />
-        </group>
+            <Float position={[0, 2.15, 0]} speed={2} rotationIntensity={2} floatIntensity={2}>
+              <mesh castShadow receiveShadow>
+                <torusKnotGeometry args={[1, 0.25, 256, 24, 1, 3]} />
+                <meshStandardMaterial color="white" roughness={0.1} metalness={0.925} />
+              </mesh>
+            </Float>
+            <ContactShadows scale={10} blur={3} opacity={0.25} far={10} />
+          </group>
+
+          <mesh castShadow receiveShadow position={[0, 0, 0]}>
+            <meshStandardMaterial color={[43/255 * 1, 26/255 * 1, 56/255 * 1]} wireframe={false} />
+            <boxGeometry args={[20, 0.5, 20, 20, 1, 20]} />
+            <Outlines thickness={5} color="hotpink" />
+          </mesh>
+
+          <mesh castShadow receiveShadow position={[0, 0.3, 0]} rotation={[Math.PI/2, 0, 0]}>
+            <meshStandardMaterial color={[255/255 * 4, 255/255 * 4, 255/255 * 4]} wireframe={true} />
+            <planeGeometry args={[20, 20, 20, 20]} />
+          </mesh>
+
+          {/*<mesh>
+            <boxGeometry />
+            <meshBasicMaterial  color={[1, 2, 0]} />
+            <Outlines thickness={2} color="hotpink" />
+          </mesh>*/}
+
+          <EffectComposer>
+            <Bloom mipmapBlur luminanceThreshold={1} />
+          </EffectComposer>
 
         </Suspense>
          { import.meta.env.MODE === "development" && (<>
@@ -58,7 +83,15 @@ function ThreejsRenderer({
           </>)
 
         }
-        <OrbitControls makeDefault maxDistance={20} autoRotate={true} autoRotateSpeed={0.25} enableZoom={true} enableRotate={true} enablePan={false} />
+        <OrbitControls
+          makeDefault
+          maxDistance={20}
+          //autoRotate={true}
+          autoRotateSpeed={0.25}
+          enableZoom={true}
+          enableRotate={true}
+          enablePan={false}
+        />
       </Canvas>
     </div>
   );
