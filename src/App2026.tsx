bb06c312cdcf456ff85c0445ac6c610de3d64d03
Guillaume Gomez <guillaume.gomez846@gmail.com>
2026-02-25 08:28:47 +0100

    remove unused html tag

--- src/App2026.tsx
+++ src/App2026.tsx
@@ -3,17 +3,16 @@ import LayoutContainer from "./LayoutContainer";
 //import Sketch from "./2026/one/Sketch";
 //import Sketch from "./2026/three/Sketch";
 //import Sketch from "./2026/five/Sketch";
-import Sketch from "./2026/eight/ThreeJsRenderer";
+//import Sketch from "./2026/eight/ThreeJsRenderer";
+import Sketch from "./2026/seven/Sketch";
 
 
 function App2026() {
   return (
     <div className="flex flex-col h-full gap-2 bg-neutral-900 text-white p-4">
       <LayoutContainer>
-        <Sketch></Sketch>
+        <Sketch/>
       </LayoutContainer>
-      <p>project for the 2026 version</p>
-        For now nothing :)
     </div>
   )
 }
