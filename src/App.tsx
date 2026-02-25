63443f1415ea84557fff7dfd6f012e6d4294d1d3
Guillaume Gomez <guillaume.gomez846@gmail.com>
2026-02-25 08:28:16 +0100

    comment partially

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,7 @@ import App2026 from "./App2026";
 function App() {
   return (
     <div className="flex flex-col h-full gap-2 bg-neutral-900 text-white p-4">
-      <App2025 />
+      {/*<App2025 />*/}
       <App2026 />
       <footer>Made by Guillaume Gomez</footer>
     </div>
